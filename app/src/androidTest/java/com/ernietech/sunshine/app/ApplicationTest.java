b369e125ce790f7f84f8110ef7b3afb60257d82f
ernie@ernietech.com <ernie@ernietech.com>
2015-06-20 13:44:50 -0400

    Change package name to reflect personal domain

--- app/src/androidTest/java/com/ernietech/sunshine/app/ApplicationTest.java
+++ app/src/androidTest/java/com/ernietech/sunshine/app/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app;
+package com.ernietech.sunshine.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/ernietech/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ernietech/sunshine/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app;
+package com.ernietech.sunshine.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
