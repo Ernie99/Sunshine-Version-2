c613c702ac39913b1e4aa15f03dd7725dd71383e
ernie@ernietech.com <ernie@ernietech.com>
2015-06-20 18:35:16 -0400

    1.06 hook the adapter up to the ListView

--- app/src/main/java/com/ernietech/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ernietech/sunshine/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -81,6 +82,9 @@ public class MainActivity extends ActionBarActivity {
                             R.id.list_item_forecast_textview,
                             weekForcast);
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ListView listView = (ListView)rootView.findViewById(R.id.listview_forcast);
+            listView.setAdapter(mForcastAdapter);
             return rootView;
         }
     }
