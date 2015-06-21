f0d6b5e76b8c80b247dd981577261bbe1e469395
ernie@ernietech.com <ernie@ernietech.com>
2015-06-21 13:17:14 -0400

    3.00 remove verbose logging statements

--- app/src/main/java/com/ernietech/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/ernietech/sunshine/app/ForecastFragment.java
@@ -183,10 +183,7 @@ public class ForecastFragment extends Fragment {
                 highAndLow = formatHighLows(high, low);
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
-
-            for (String s : resultStrs) {
-                Log.v(LOG_TAG, "Forecast entry: " + s);
-            }
+            
             return resultStrs;
 
         }
@@ -226,8 +223,6 @@ public class ForecastFragment extends Fragment {
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
                         .build();
                 URL url = new URL(builtUri.toString());
-
-                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
