52d0637258e917dc5be517379fc5f53ab67c871f
ernie@ernietech.com <ernie@ernietech.com>
2015-06-20 20:03:17 -0400

    2.04 inflate menu

--- app/src/main/java/com/ernietech/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/ernietech/sunshine/app/ForecastFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -28,6 +30,17 @@ public class ForecastFragment extends Fragment {
     ArrayAdapter<String> mForcastAdapter;
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
     }
 
     @Override
