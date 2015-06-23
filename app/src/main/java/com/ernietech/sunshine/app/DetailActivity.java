ec8fdd6ff921a02b60fb65437115afe7c0dddf2b
ernie@ernietech.com <ernie@ernietech.com>
2015-06-23 07:52:41 -0400

    create share button, but doesnt work

--- app/src/main/java/com/ernietech/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/ernietech/sunshine/app/DetailActivity.java
@@ -3,12 +3,17 @@ package com.ernietech.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
@@ -54,7 +59,12 @@ public class DetailActivity extends ActionBarActivity {
     public static class DetailFragment extends Fragment {
 
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String FORCAST_SHARE_HASHTAG = " #SunshineApp";
+        private String mForecastStr;
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,11 +73,33 @@ public class DetailActivity extends ActionBarActivity {
             Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-                String forcastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
                 ((TextView) rootView.findViewById(R.id.detail_text))
-                        .setText(forcastStr);
+                        .setText(mForecastStr);
             }
             return rootView;
+        }
+
+        private Intent createShareForecastIntent(){
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mForecastStr + FORCAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment, menu);
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            if(mShareActionProvider != null ){
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }else{
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
         }
     }
 }
